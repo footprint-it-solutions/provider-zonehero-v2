2db4f7b92c7d709f63c213ce242485e9bdb70c5f
Alex Sunins <alex@footprintit.net>
2025-09-04 15:30:42 +0100

    test generate run

---
+++ internal/controller/cluster/zz_setup.go
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controller
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/crossplane/upjet/v2/pkg/controller"
+
+	heroloadbalancerlistenerattachment "github.com/footprint-it-solutions/provider-zonehero-v2/internal/controller/cluster/hlb/heroloadbalancerlistenerattachment"
+	providerconfig "github.com/footprint-it-solutions/provider-zonehero-v2/internal/controller/cluster/providerconfig"
+)
+
+// Setup creates all controllers with the supplied logger and adds them to
+// the supplied manager.
+func Setup(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		heroloadbalancerlistenerattachment.Setup,
+		providerconfig.Setup,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetupGated creates all controllers with the supplied logger and adds them to
+// the supplied manager gated.
+func SetupGated(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		heroloadbalancerlistenerattachment.SetupGated,
+		providerconfig.SetupGated,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
